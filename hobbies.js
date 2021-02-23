f0733c8af2cb94cb089e381f72ec4e0027e281af
nlanners <lannersn@oregonstate.edu>
2021-02-22 18:33:01 -0700

    Initial commit

---
+++ hobbies.js
@@ -0,0 +1,61 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');    //DOUBLE CHECK WHETHER THIS IS NEEDED
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 1111);
+
+app.get('/', function(req, res){
+    var context = {};
+    context.title = "Home Page";
+    //document.getElementsByClassName('active').className=null;
+    //document.getElementById('home').className='active';
+    res.render('home', context);
+});
+
+app.get('/books', function(req, res){
+    var context = {};
+    context.title = "Books";
+    //document.getElementsByClassName('active').className=null;
+    //document.getElementById('books').className='active';
+    res.render('books', context);
+});
+
+app.get('/board-games', function(req, res){
+    var context = {};
+    context.title = "Board Games";
+    //document.getElementsByClassName('active').className=null;
+    //document.getElementById('board-games').className='active';
+    res.render('board-games', context);
+});
+
+app.get('/painting', function (req, res){
+    var context = {};
+    context.title = "Painting";
+    //document.getElementsByClassName('active').className=null;
+    //document.getElementById('painting').className='active';
+    res.render('painting', context);
+});
+
+app.use(function(req, res){
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
---
+++ public/js/main.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", setActive());
+
+function setActive() {
+    if (document.getElementById("title").textContent === "Home Page") {
+        document.getElementById("home").className = "active";
+    } else if (document.getElementById("title").textContent === "Books") {
+        document.getElementById("books").className = "active";
+    } else if (document.getElementById("title").textContent === "Board Games") {
+        document.getElementById("board-games").className = "active";
+    } else if (document.getElementById("title").textContent === "Painting") {
+        document.getElementById("painting").className = "active";
+    }
+}
+
+var slideIndex = 0;
+autoShowSlides();
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" activeDot", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " activeDot";
+}
+
+function autoShowSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(autoShowSlides, 10000);
+}
